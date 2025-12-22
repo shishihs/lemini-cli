84c72fc5c14bc88ab5b3c3e0099ee6eb7946c37c
Takafumi Terashi <shishi@TakafuminoMacBook-Air.local>
2025-12-22 17:06:10 +0900

    Suppress punycode deprecation warning

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -11,6 +11,22 @@ import { FatalError, writeToStderr } from '@google/gemini-cli-core';
 import { runExitCleanup } from './src/utils/cleanup.js';
 
 // --- Global Entry Point ---
+
+// Suppress the punycode deprecation warning caused by dependencies
+// TODO: Remove this once dependencies are updated
+const originalEmit = process.emit;
+process.emit = function (name: any, data: any, ...args: any[]) {
+  if (
+    name === 'warning' &&
+    typeof data === 'object' &&
+    data?.name === 'DeprecationWarning' &&
+    data?.message?.includes('The `punycode` module is deprecated')
+  ) {
+    return false;
+  }
+  return originalEmit.apply(process, [name, data, ...args]);
+} as any;
+
 main().catch(async (error) => {
   await runExitCleanup();
 
